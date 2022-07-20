05943f55a940ee088855c166b8eb939625acb38f
jayzumen <reinhardtjanniklas96@gmail.com>
2022-07-20 14:44:00 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,118 @@
+import { useEffect, useRef, useState } from "react";
+import { nanoid } from "nanoid";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import Todo from "./components/Todo";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
-  return (
-    <div className="todoapp stack-large">
-      <h1>Todo App</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What need to be done?
-          </label>
-        </h2>
-        <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off" />
-        <button type="submit" className="btn btn__primary btn__lg">Add</button>
-      </form>
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true" >
-          <span className="visually-hidden" >Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  };
+
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  };
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const addTask = (name) => {
+    const newTask = {
+      id: "todo-" + nanoid(),
+      name: name,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
+  return (
+    // Heading and Input
+    <div className='todoapp stack-large'>
+      <h1>Todo App</h1>
+      <Form addTask={addTask} />
+
+      {/* Filter */}
+      <div className='filters btn-group stack-exception'>
+        {filterList}
       </div>
 
-      <h2 className="list-heading">3 tasks remaining</h2>
-      <ul className="todo-list stack-large stack exception" aria-labelledby="list-heading" >
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input type="checkbox" id="todo-0" defaultChecked={true} />
-            <label htmlFor="todo-0" className="todo-label">Eat</label>
-          </div>
-        </li>
+      {/* TODOS */}
+      <h2 className='list-heading' tabIndex='-1' ref={listHeadingRef}>
+        {headingText}
+      </h2>
+      <ul
+        className='todo-list stack-large stack exception'
+        aria-labelledby='list-heading'
+      >
+        {taskList}
       </ul>
-      
     </div>
   );
 }
